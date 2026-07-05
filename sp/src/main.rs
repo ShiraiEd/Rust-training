f4c39c6037da2176a8ccfe7b2a90285dc04ebb15
Eduardo Egydio Shirai <eduardoegydioshirai599@gmail.com>
2026-07-05 18:25:45 +0900

    Making Rc mutable with RefCell interior muttability

--- sp/src/main.rs
+++ sp/src/main.rs
@@ -24,25 +24,36 @@
 //        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
 //    }
 //}
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 use crate::List::{Cons, Nil};
+use std::cell::{Ref, RefCell};
 use std::rc::Rc;
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(2, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!(
-        "count after c goee put of the scope = {}",
-        Rc::strong_count(&a)
-    );
+    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
+    //    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    //    println!("count after creating a = {}", Rc::strong_count(&a));
+    //    let b = Cons(2, Rc::clone(&a));
+    //    println!("count after creating b = {}", Rc::strong_count(&a));
+    //    {
+    //        let c = Cons(4, Rc::clone(&a));
+    //        println!("count after creating c = {}", Rc::strong_count(&a));
+    //    }
+    //    println!(
+    //        "count after c goee put of the scope = {}",
+    //        Rc::strong_count(&a)
+    //    );
     //  let x = 5;
     //  let y = MyBox::new(x);
     //
