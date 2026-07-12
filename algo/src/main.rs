14dacf9ed3f3419a25cb42d058ce308ea73a0d55
Eduardo Egydio Shirai <eduardoegydioshirai599@gmail.com>
2026-07-12 20:54:12 +0900

    Improved linear and binary search by returning an Option

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,35 +1,49 @@
 fn main() {
     let arr = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let arr2: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    println!("primeira busca l: {}", linear_search(&arr, 6));
-    println!("segunda busca l: {}", linear_search(&arr, 8));
-    println!("terceira busca l: {}", linear_search(&arr, 11));
-    println!("busca binaria: {}", binary_search(&arr, 4));
-    println!("segunda busca b: {}", binary_search(&arr, 2));
-    println!("terceira busca b: {}", binary_search(&arr, 3));
-    println!("quarta busca b: {}", binary_search(&arr, 6));
+
+    for i in arr2 {
+        match linear_search(&arr2, &i) {
+            Some(posicao) => println!("Linear: item da array: {i}\nPosicao: {}", posicao),
+            None => println!("Item ao encontrado na array"),
+        }
+        println!(" ");
+    }
+
+    println!("-------------------");
+    println!(" ");
+
+    for i in &arr {
+        match binary_search(&arr, i) {
+            Some(posicao) => println!("Binary: item da array: {i}\nPosicao: {}", posicao),
+            None => println!("Item nao encontrado na array"),
+        }
+        println!(" ");
+    }
 }
 
-fn linear_search(arr: &[i32], item: i32) -> isize {
+fn linear_search(arr: &[i32], item: &i32) -> Option<isize> {
     for (idx, valor) in arr.iter().enumerate() {
-        if *valor == item {
-            return idx as isize;
+        if valor == item {
+            return Some(idx as isize);
         }
     }
-    -1
+    None
 }
 
-fn binary_search(arr: &[i32], item: i32) -> i32 {
+fn binary_search(arr: &[i32], item: &i32) -> Option<i32> {
     let metade = ((arr.len() as f64 / 2_f64).ceil() - 1_f64) as usize;
-    if arr[metade] == item {
-        return metade as i32;
-    }
-    if arr[metade] < item {
-        let slice = &arr[metade + 1..];
-        binary_search(slice, item) + metade as i32 + 1_i32
-    } else {
-        let slice = &arr[..metade];
-        binary_search(slice, item)
+    match arr[metade].cmp(item) {
+        std::cmp::Ordering::Equal => Some(metade as i32),
+
+        std::cmp::Ordering::Less => {
+            let slice = &arr[metade + 1..];
+            Some(binary_search(slice, item).unwrap() + metade as i32 + 1)
+        }
+        std::cmp::Ordering::Greater => {
+            let slice = &arr[..metade];
+            binary_search(slice, item)
+        }
     }
     //     let mut metade = (arr.len() as f64 / 2_f64).round() as usize;
     //     while arr[metade] != item {
